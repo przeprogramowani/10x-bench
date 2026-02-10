b2647e6f15a13e76c2e794814e96756258d263e8
psmyrdek <przemek.smyrdek@gmail.com>
2026-02-10 10:34:10 +0100

    add social media cards

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://10xbench.ai',
   integrations: [react(), tailwind()],
   output: 'static',
 });
